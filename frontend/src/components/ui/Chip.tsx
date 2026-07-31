1d548b0e58b0ded828c5c063e71de32880b2283e
seba <63276982+sebaa1@users.noreply.github.com>
2026-07-30 21:22:32 -0300

    feat: add disabled state styling

--- frontend/src/components/ui/Chip.tsx
+++ frontend/src/components/ui/Chip.tsx
@@ -1,12 +1,15 @@
-
 interface ChipProps {
     text: string;
     icon: React.ElementType;
+    isDisabled?: boolean;
 }
 
-export function Chip({ text, icon: Icon }: ChipProps) {
+export function Chip({ text, icon: Icon, isDisabled = false }: ChipProps) {
     return (
-        <div className="flex items-center gap-2 bg-white px-4 py-2.5 rounded-xl text-black">
+        <div
+            className={`flex items-center gap-2 bg-white px-6 py-2 rounded-2xl text-black transition-opacity ${isDisabled ? "opacity-25" : "opacity-100"
+                }`}
+        >
             <Icon className="size-5" />
             <span>{text}</span>
         </div>
